908a93aeb7e4a51c3fa3f1d3cbb425c2b93b77ff
Asuka-I <1400cm@gmail.com>
2021-07-26 06:21:55 +0900

    createアクション リクエストスペックcreateを作成

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  # 一覧ページ
   describe "GET #index" do
     subject { get(users_path) }
+
     context "ユーザーが存在するとき" do
       before { create_list(:user, 3) }
+
       it "リクエストが成功する" do
         subject
         expect(response).to have_http_status(:ok)
@@ -16,11 +19,14 @@ RSpec.describe "Users", type: :request do
     end
   end
   
+  # 詳細ページ
   describe "GET #show" do
     subject { get(user_path(user_id)) }
+
     context "ユーザーが存在するとき" do
       let(:user) { create(:user) }
       let(:user_id) { user.id }
+
       context ":idに対応するユーザーが存在しないとき" do
         let(:user_id) { 1 }
 
@@ -51,11 +57,54 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  # 新規投稿ページ
   describe "GET #new" do
     subject { get(new_user_path) }
+
     it "リクエストが成功する" do
       subject
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  # 新規投稿機能
+  describe "POST #create" do
+    subject { post(users_path, params: params) }
+
+    context "パラメータが正常なとき" do
+      let(:params) { { user: attributes_for(:user) } }
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(302)
+      end
+
+      it "ユーザーが保存される" do
+        expect { subject }.to change { User.count }.by(1)
+      end
+
+      it "詳細ページにリダイレクトされる" do
+        subject
+        expect(response).to redirect_to User.last
+      end
+    end
+
+    context "パラメータが異常なとき" do
+      let(:params) { { user: attributes_for(:user, :invalid) } }
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(200)
+      end
+
+      it "ユーザーが保存されない" do
+        expect { subject }.not_to change(User, :count)
+      end
+
+      it "新規投稿ページがレンダリングされる" do
+        subject
+        expect(response.body).to include "新規投稿"
+      end
     end
   end
 end
